094477e128e23eb16028c7e6594b1909773c7dac
Соловьев Роман <serrcel@gmail.com>
2022-08-04 15:48:04 +0400

    (upd) Added new models: 1) Teacher 2) Lesson

--- UniversitySchedule/schedule/models.py
+++ UniversitySchedule/schedule/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+
+class Teacher(models.Model):
+    """"
+    Модель преподователя. Содержит фамилию и имя преподователя. Имеется контакная информация
+    """
+    teacher_name = models.CharField('Имя', max_length=255)
+    teacher_lastname = models.CharField('Фамилия', max_length=255)
+    email = models.EmailField('Электронная почта', max_length=254)
+    eeistu_url = models.SlugField(max_length=250)
+
+    def __str__(self):
+        return f'{self.teacher_lastname} {self.teacher_name}'
+
+    class Meta:
+        verbose_name = 'Преподаватель'
+        verbose_name_plural = 'Преподаватели'
+
+
+
+class Lesson(models.Model):
+    """"
+    Модель пары
+    """
+    lesson_name = models.CharField('Пара', max_length=255)
+    lesson_type = models.CharField('Тип пары', max_length=255)
+    lesson_auditory = models.CharField('Кабинет', max_length=255)
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+
+    def __str__(self):
+        return f'{self.lesson_type} {self.lesson_name}'
+
+    class Meta:
+        verbose_name = 'Пара'
+        verbose_name_plural = 'Пары'
